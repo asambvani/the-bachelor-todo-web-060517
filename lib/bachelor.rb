ff7adee50ea24a9195f4b933c94c7f734c2c97cb
Alex Sambvani <alexsambvani@gmail.com>
2017-05-29 23:29:57 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |object|
+    if object["status"] == "Winner"
+      return object["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_key, season_value|
+    season_value.each do |object|
+      if object["occupation"] == occupation
+        return object["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_key, season_value|
+    season_value.each do |object|
+      if object["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_key, season_value|
+    season_value.each do |object|
+      if object["hometown"] == hometown
+        return object["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  count = 0
+  data[season].each do |object|
+    age_array << object["age"].to_i
+  end
+  age_array.each do |item|
+    count += item
+  end
+  puts "Count: #{count} Size: #{age_array.size}"
+  puts age_array
+
+  return (count.to_f / age_array.length.to_f).round(0)
 end
